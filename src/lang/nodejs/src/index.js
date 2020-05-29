c8f67af2d4a20fe77a2bf1680979883e1a507423
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-05-29 18:06:31 +0800

    Make code style changes and move files to nodejs folder

--- src/lang/nodejs/src/index.js
+++ src/lang/nodejs/src/index.js
@@ -1,50 +1,48 @@
 const http = require("http");
 const open = require("open");
 const fs = require("fs");
-const WebSocketServer = require("websocket").server; // websocket server class
+const WebSocketServer = require("websocket").server;
+
+/* CONSTANTS */
+
+// Frontend file paths
+const HTML_FILE_PATH = "../frontend.html";
+const JS_FILE_PATH = "frontend.js";
+
+const DEFAULT_PORT = 9101;
+const HOST = "http://127.0.0.1";
+
+/* NETWORKING VARIABLES */
 
 let webSocket = null;
-// connection variable, to refer to the web socket connection to the client once connection is established
-let connection = null;
-// default port used if none is specified by the user
-let defaultPort = 9101;
-const host = "http://127.0.0.1";
+let connection = null; // refers to the web socket connection to the client once connection is established
+let isRunning = false; // track if a browser window is already open
 
-// check if a browser window is already open
-let isRunning = false;
-
-
-exports.visualise = function visualise(jsonData, port = defaultPort) {
-
+/**
+ * Displays json data in a browser
+ * @param {String} jsonStr Stringified JSON data to be viewed
+ * @param {Number} port Port which the user wants to use for the network connection between browser and server. Default port of 9101 will be used if not provided by user
+ */
+exports.visualise = function visualise(jsonStr, port = DEFAULT_PORT) {
   // overhead of server creation only done on first call
   if (!isRunning) {
     isRunning = true;
 
-    // open index.html in the browser
-    open("http://127.0.0.1:" + port);
-
-    // create http Server
     const httpServer = http.createServer((req, res) => {
-
-      // check if the which files to render
-      var url = req.url;
-
-      switch (url) {
+      switch (req.url) {
         case '/':
-          getStaticFileContent(res, 'public/index.html', 'text/html');
+          renderFile(res, HTML_FILE_PATH, 'text/html');
           break;
-        case '/public/test_websocket.js':
-          getStaticFileContent(res, 'public/test_websocket.js', 'text/javascript');
+        case JS_FILE_PATH:
+          renderFile(res, JS_FILE_PATH, 'text/javascript');
           break;
         default:
-
           res.writeHead(404, {
             'Content-Type': 'text/plain'
           });
           res.end('404 - Page not Found');
           break;
       }
-
     });
 
     httpServer.listen(port, (req, res) => {
@@ -56,33 +54,33 @@ exports.visualise = function visualise(jsonData, port = defaultPort) {
     });
 
     webSocket.on("request", request => {
-
-      // accept request
       connection = request.accept(null, request.origin);
 
-      connection.send(JSON.stringify(jsonData));
-
       connection.on("close", () => console.log("Connection closed"));
-      connection.on("message", message => console.log("hey now brown cow"));
+      connection.on("message", message => console.log(message));
 
+      connection.send(jsonStr);
     });
 
+    // show index.html in the browser
+    open("http://127.0.0.1:" + port);
+
   } else if (connection == null) {
-    setTimeout(() => visualise(jsonData, port), 500);
+    setTimeout(() => visualise(jsonStr, port), 500);
   } else {
-    connection.send(JSON.stringify(jsonData));
+    connection.send(jsonStr);
   }
-
-  // connection.send(jsonData, connection);
-  // sendData(jsonData, connection);
-
 }
 
-// From StackOverflow
-// for rendering static files
-function getStaticFileContent(response, filepath, contentType) {
-  fs.readFile(filepath, function(error, data) {
-    if (error) {
+/**
+ * Renders a file as a response to a request
+ * @param {*} response Object representing the response
+ * @param {String} filepath Path to the file to be rendered
+ * @param {String} contentType The media type of the file
+ */
+function renderFile(response, filepath, contentType) {
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
       response.writeHead(500, {
         'Content-Type': 'text/plain'
       });
