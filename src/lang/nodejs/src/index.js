209e76c7749a07ee1660718039869b76f0a69a9e
Saad Haider <saadh98@gmail.com>
2020-05-31 08:14:23 +0800

    Separate server and websocket responsibilites into separate functions. Create separate function for setting the port

--- src/lang/nodejs/src/index.js
+++ src/lang/nodejs/src/index.js
@@ -14,9 +14,11 @@ const HOST = "http://127.0.0.1";
 
 /* NETWORKING VARIABLES */
 
+const httpServer = null;
 let webSocket = null;
 let connection = null; // refers to the web socket connection to the client once connection is established
 let isRunning = false; // track if a browser window is already open
+let port = DEFAULT_PORT;
 
 /**
  * Displays json data in a browser
@@ -28,47 +30,18 @@ exports.visualise = function visualise(jsonStr, port = DEFAULT_PORT) {
   if (!isRunning) {
     isRunning = true;
 
-    const httpServer = http.createServer((req, res) => {
-      switch (req.url) {
-        case '/':
-          renderFile(res, HTML_FILE_PATH, 'text/html');
-          break;
-        case JS_FILE_PATH:
-          renderFile(res, JS_FILE_PATH, 'text/javascript');
-          break;
-        default:
-          res.writeHead(404, {
-            'Content-Type': 'text/plain'
-          });
-          res.end('404 - Page not Found');
-          break;
-      }
-    });
+    startServer(port);
+    setWebsocket();
 
-    httpServer.listen(port, (req, res) => {
-      console.log("Server listening on port " + port);
-      
-      // show index.html in the browser
-      open("http://127.0.0.1:" + port);
-    });
-
-    webSocket = new WebSocketServer({
-      "httpServer": httpServer
-    });
-
-    webSocket.on("request", request => {
-      connection = request.accept(null, request.origin);
-
-      connection.on("close", () => console.log("Connection closed"));
-      connection.on("message", message => console.log(message));
-
-      connection.send(jsonStr);
-    });
   } else if (!connection) {
     setTimeout(() => visualise(jsonStr, port), 500);
   } else {
     connection.send(jsonStr);
   }
+}
+
+exports.setPort = function(customPort) {
+  port = customPort;
 }
 
 /**
@@ -90,5 +63,53 @@ function renderFile(response, filepath, contentType) {
       });
       response.end(data, 'utf-8');
     }
+  });
+}
+
+/**
+ * Creates and sets up a server which listens on the specified port
+ * @param {Number} port Port on which the server listens
+ */
+function startServer(port) {
+  httpServer = http.createServer((req, res) => {
+    switch (req.url) {
+      case '/':
+        renderFile(res, HTML_FILE_PATH, 'text/html');
+        break;
+      case JS_FILE_PATH:
+        renderFile(res, JS_FILE_PATH, 'text/javascript');
+        break;
+      default:
+        res.writeHead(404, {
+          'Content-Type': 'text/plain'
+        });
+        res.end('404 - Page not Found');
+        break;
+    }
+  });
+
+  httpServer.listen(port, (req, res) => {
+    console.log("Server listening on port " + port);
+
+    // show index.html in the browser
+    open("http://127.0.0.1:" + port);
+  });
+}
+
+/**
+ * Sets up the websocket on the server end. Defines event handlers for web socket connection.
+ */
+function setWebsocket() {
+  webSocket = new WebSocketServer({
+    "httpServer": httpServer
+  });
+
+  webSocket.on("request", request => {
+    connection = request.accept(null, request.origin);
+
+    connection.on("close", () => console.log("Connection closed"));
+    connection.on("message", message => console.log(message));
+
+    connection.send(jsonStr);
   });
 }
