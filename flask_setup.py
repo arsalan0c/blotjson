f0a248f687be0e3bdda94d861a5a94a9d472be13
usmansiddiqui98 <e0412913@u.nus.edu>
2020-06-07 04:05:35 +0800

    reformatting

--- flask_setup.py
+++ flask_setup.py
@@ -3,16 +3,19 @@ import json
 import webbrowser
 from threading import Timer
 
-##check if file is json
+
+#check if file is json
 def is_json(my_json):
     try:
-        json_object=json.loads(my_json)
+        json_object = json.loads(my_json)
     except ValueError as e:
         return False
     return True
 
+
 # initialize Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -21,7 +24,9 @@ def index():
 
 
 def open_browser():
-    webbrowser.open_new('http://127.0.0.1:9101/')
+    webbrowser.open_new('http://127.0.0.1:1235/')
+
+
 if __name__ == "__main__":
-      Timer(1, open_browser).start();
-      app.run(port=2000)
+    Timer(1, open_browser).start();
+    app.run(port=1235)
