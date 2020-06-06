5063d0d3433b3fa0a35bc0d6212106942f0b7ebd
usmansiddiqui98 <e0412913@u.nus.edu>
2020-06-07 03:59:53 +0800

    added flask dependency, thinking of srapping async connection

---
+++ flask_setup.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import json
+import webbrowser
+from threading import Timer
+
+##check if file is json
+def is_json(my_json):
+    try:
+        json_object=json.loads(my_json)
+    except ValueError as e:
+        return False
+    return True
+
+# initialize Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Serve the index HTML"""
+    return render_template('index.html')
+
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:9101/')
+if __name__ == "__main__":
+      Timer(1, open_browser).start();
+      app.run(port=2000)
--- ws_server.py
+++ ws_server.py
@@ -2,14 +2,21 @@ import asyncio
 import websockets
 import json
 
+##check if file is json
+def is_json(my_json):
+    try:
+        json_object=json.loads(my_json)
+    except ValueError as e:
+        return False
+    return True
 
 async def response(websocket, path):
     message = await websocket.recv()
     print(f"we got the message from the client!: {message}")
     await websocket.send("I can confirm I got your message!")
-
-start_server = websockets.serve(response, 'localhost', 1234)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ = '__main__':
+    start_server = websockets.serve(response, 'localhost', 1234)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
 
 
