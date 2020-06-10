bed66c6b1498bcce7c4499f0007b311b8c61057a
usmansiddiqui98 <e0412913@u.nus.edu>
2020-06-10 21:09:20 +0800

    decided on asyncio

--- ws_server.py
+++ ws_server.py
@@ -1,22 +1,26 @@
 import asyncio
 import websockets
 import json
+import random
+import datetime
+
 
 ##check if file is json
 def is_json(my_json):
     try:
-        json_object=json.loads(my_json)
+        json_object = json.loads(my_json)
     except ValueError as e:
         return False
     return True
 
-async def response(websocket, path):
-    message = await websocket.recv()
-    print(f"we got the message from the client!: {message}")
-    await websocket.send("I can confirm I got your message!")
-if __name__ = '__main__':
-    start_server = websockets.serve(response, 'localhost', 1234)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+
+async def time(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + "Z"
+        await websocket.send(now)
+        await asyncio.sleep(random.random() * 3)
 
 
+start_server = websockets.serve(time, "127.0.0.1", 5678)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
