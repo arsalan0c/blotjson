15b5affacea16e150d3a57d6b001b5c111870574
umergta <umergta@gmail.com>
2020-06-17 01:28:59 +0800

    added buttons with collapse functionality

--- frontend.js
+++ frontend.js
@@ -1,10 +1,8 @@
 const host = window.location.host;
-var data = "input test..";
 
 var webSocket = new WebSocket("ws://" + host);
-webSocket.onmessage = function(){
+webSocket.onmessage = function () {
     console.log("server connected");
-    //document.getElementById("input-area").innerHTML += "<div>" + data + "</div>";
 };
 
 var htmlData = "";
@@ -15,31 +13,75 @@ var webSocket = new WebSocket("ws://" + host);
 
 webSocket.onmessage = function (json) {
     allJsonObjects.push(JSON.parse(json.data));
-    console.log(allJsonObjects);
     displayData();
 }
 
 function jsonConversion(jsonData) {
-
     var input = "";
 
     for (item in jsonData) {
-        input += "<div .individual-data>" + item + "<hr>" + "</div>";
+        input += "<button class= 'text-collapse-btn'>&#9660</button>"
+        input += "<div class= 'individual-data' 'collapse'>" + item + ":" + "</div>";
         if (typeof (jsonData[item]) === "object") {
-            input += jsonConversion(jsonData[item]);
+            input += "<br>" + jsonConversion(jsonData[item]);
         } else {
-            input += jsonData[item];
+            input += "<div class= 'data-inside'>" + jsonData[item] + "</div>";
         }
     }
     return input;
 }
-
+function buttonCollapse() {
+    let collapsers = document.getElementsByClassName("text-collapse-btn");
+    //console.log(collapsers.length);
+    var collapsableData = document.querySelectorAll(".individual-data");
+    console.log(Array.from(collapsableData));
+    console.log(collapsableData);
+    for (let i = 0; i < collapsers.length; i++) {
+        console.log("test");
+        collapsers[i].addEventListener("click", function () {
+            console.log("clicked");
+            //add collapse for this click to all text below
+            for (let j = i; j < collapsableData.length; j++) {
+                collapsableData[j].classList.toggle("collapse");
+            }
+        });
+        //reduce size of array for every iteration as we go down the column of buttons
+        //so only the text below collapses
+        
+        //console.log(collapsableData);
+    }
+}
 function displayData() {
     htmlData = "";
 
     allJsonObjects.forEach(data => {
-        htmlData += jsonConversion(data);
+        htmlData += jsonConversion(data) + "<hr>";
     });
 
     document.getElementById("input-area").innerHTML = htmlData;
+    buttonCollapse();
 }
+
+
+// function jsonCoversion2(jsonData) {
+//     var input = "<table>";
+//     for (item in jsonData) {
+//         input += "<div class= 'individual-data'>" + "<tr>" + "<td>" + "<button>↑</button>" + "</td> " + "<td>" + item + ":" + "</td> </div>";
+//         if (typeof (jsonData[item]) === "object") {
+//             input += "<tr>" + jsonConversion(jsonData[item]) + "</tr>";
+//         } else {
+//             input += "<div class= 'data-inside'>"+"<td>" + jsonData[item] + "</td>" + "</tr>" + "</div>";
+//         }
+//     }
+//     input += "</table>"
+//     return input;
+// }
+// function displayData2() {
+//     htmlData = "";
+
+//     allJsonObjects.forEach(data => {
+//         htmlData += jsonConversion2(data);
+//     });
+//     htmlData += ""
+//     document.getElementById("input-area").innerHTML = htmlData;
+// }
