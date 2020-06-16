5d1dd47689ab2b17c668c747b6142c58ce2932ce
umergta <umergta@gmail.com>
2020-06-17 01:30:15 +0800

    removed useless comments

--- darkTheme.js
+++ darkTheme.js
@@ -5,8 +5,8 @@ themeButton.addEventListener("click", function(){
     clickCount++;
     var image = document.querySelector(".main-logo");
     if(clickCount % 2 === 0){
-        image.src="images/Frame 1.svg";
+        image.src="Frame_1.svg";
     }else{
-        image.src= "images/Frame 2-3.svg";
+        image.src= "Frame_2-3.svg";
     }    
 });
