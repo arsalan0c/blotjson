06c7736c0eb4fe478f42d5d4c357681cf7b53c09
umergta <umergta@gmail.com>
2020-06-17 01:22:23 +0800

    modified test file

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
-var content = {
-    "firstName" : "Umer",
-    "LastName" : "Siddiqui"
+var blot = require("./index.js");
+var testData = {
+    "object a": "string a",
+    "object nest": {
+        "nested object": "string nested in object b"
+    },
+    "object b" : "string b"
 }
-var output = document.getElementById('output');
-output.innerHTML = content.firstName + content.LastName;
+
+var testData2 = {
+    "second object" : 12345,
+    "array object" : ["one", "two" ,"three"]
+}
+blot.visualise(testData);
+// blot.visualise(testData2)
+// console.log(typeof([1,2,3]));
