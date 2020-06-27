3cc798503c836e7c845c7713c89c29de2295af1a
umergta <umergta@gmail.com>
2020-06-28 05:18:12 +0800

    added functionality for expand-contract button and added comments

--- frontend.js
+++ frontend.js
@@ -1,20 +1,29 @@
+//constants and variables
 const host = window.location.host;
-
 var webSocket = new WebSocket("ws://" + host);
-webSocket.onmessage = function () {
-    console.log("server connected");
-};
-
 var htmlData = "";
 var allJsonObjects = [];
+
 
 // establish websocket connection between frontend and backend
 var webSocket = new WebSocket("ws://" + host);
 
+/*
+Upon the onmessage event of a websocket, the parsed JSON data is pushed into the
+allJsonObjects array
+*/
 webSocket.onmessage = function (json) {
     allJsonObjects.push(JSON.parse(json.data));
     displayData();
 }
+
+/**
+ * html rendering for every individual piece of json data
+ * key of json is added into a div with class of 'individual-data'
+ * value of json is added into a div with class of 'data-inside'
+ * @param {string} jsonData parsed JSON object
+ * @returns {string} html for the jsonData
+ */
 
 function jsonConversion(jsonData) {
     var input = "";
@@ -30,31 +39,12 @@ function jsonConversion(jsonData) {
     }
     return input;
 }
-function buttonCollapse() {
-    let collapsers = document.getElementsByClassName("text-collapse-btn");
-    //console.log(collapsers.length);
-    var collapsableData = document.querySelectorAll(".individual-data");
-    var internalCollapsableData = document.querySelectorAll(".data-inside");
-    //console.log(Array.from(collapsableData));
-    //console.log(collapsableData);
-    for (let i = 0; i < collapsers.length; i++) {
-        console.log("test");
-        collapsers[i].addEventListener("click", function () {
-            console.log("clicked");
-            collapsers[i].classList.toggle("btn-after");
-            //add collapse for this click to all text below
-            for (let j = i; j < collapsableData.length; j++) {
-                collapsableData[j].classList.toggle("collapse");
-                //length of internaldata nodelist may be longer....
-                internalCollapsableData[j].classList.toggle("collapse");
-            }
-        });
-        //reduce size of array for every iteration as we go down the column of buttons
-        //so only the text below collapses
-        
-        //console.log(collapsableData);
-    }
-}
+
+/**
+ * html for collective JSON objects to be presented 
+ * For all JSON objects in the array of parsed JSON objects
+ */
+
 function displayData() {
     htmlData = "";
 
@@ -66,26 +56,50 @@ function displayData() {
     buttonCollapse();
 }
 
+//function to add buttons for text loaded with functionality of collapsing text
+/**
+ * to add in a button for every json object that collapses the object upon a click
+ */
+function buttonCollapse() {
+    let collapsers = document.getElementsByClassName("text-collapse-btn");
+    var collapsableData = document.querySelectorAll(".individual-data");
+    var internalCollapsableData = document.querySelectorAll(".data-inside");
+    for (let i = 0; i < collapsers.length; i++) {
+        console.log("test");
+        collapsers[i].addEventListener("click", function () {
+            console.log("clicked");
+            collapsers[i].classList.toggle("btn-after");
+            //add collapse for this click to all text below
+            for (let j = i; j < collapsableData.length; j++) {
+                collapsableData[j].classList.toggle("collapse");
+                internalCollapsableData[j].classList.toggle("collapse");
+            }
+        });
+    }
+}
 
-// function jsonCoversion2(jsonData) {
-//     var input = "<table>";
-//     for (item in jsonData) {
-//         input += "<div class= 'individual-data'>" + "<tr>" + "<td>" + "<button>↑</button>" + "</td> " + "<td>" + item + ":" + "</td> </div>";
-//         if (typeof (jsonData[item]) === "object") {
-//             input += "<tr>" + jsonConversion(jsonData[item]) + "</tr>";
-//         } else {
-//             input += "<div class= 'data-inside'>"+"<td>" + jsonData[item] + "</td>" + "</tr>" + "</div>";
-//         }
-//     }
-//     input += "</table>"
-//     return input;
-// }
-// function displayData2() {
-//     htmlData = "";
+//to expand and contract the input-area
 
-//     allJsonObjects.forEach(data => {
-//         htmlData += jsonConversion2(data);
-//     });
-//     htmlData += ""
-//     document.getElementById("input-area").innerHTML = htmlData;
-// }
+var click = 0;
+var expandBtn = document.querySelector(".expand-contract-btns");
+expandBtn.addEventListener("click", function () {
+    let inputArea = document.querySelector("#input-area");
+    let collapseData = document.querySelectorAll(".individual-data");
+    let internalData = document.querySelectorAll(".data-inside");
+    click++;
+    console.log(click);
+    data = "";
+    if (click % 2 != 0) {
+        for (var i = 0; i < collapseData.length; i++) {
+            collapseData[i].innerHTML = "{...}"
+            internalData[i].innerHTML = "";
+            console.log("inside for loop");
+        }
+        //inputArea.innerHTML = "";
+        expandBtn.innerHTML = "Contract";
+    } else {
+        expandBtn.innerHTML = "Expand";
+        inputArea.innerHTML = htmlData;
+        buttonCollapse();
+    }
+});
