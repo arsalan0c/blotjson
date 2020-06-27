9506518ee987f6e76f965a597c405e8473c56fb4
umergta <umergta@gmail.com>
2020-06-28 05:15:38 +0800

    added comments

--- darkTheme.js
+++ darkTheme.js
@@ -1,12 +1,16 @@
+/*
+gives the slider in the html page functionality to change theme of the page
+*/
+
 var themeButton = document.querySelector(".slider");
 var clickCount = 0;
-themeButton.addEventListener("click", function(){
+themeButton.addEventListener("click", function () {
     document.querySelector("body").classList.toggle("darktheme");
     clickCount++;
     var image = document.querySelector(".main-logo");
-    if(clickCount % 2 === 0){
-        image.src="Frame_1.svg";
-    }else{
-        image.src= "Frame_2-3.svg";
-    }    
+    if (clickCount % 2 === 0) {
+        image.src = "Frame_1.svg";
+    } else {
+        image.src = "Frame_2-3.svg";
+    }
 });
