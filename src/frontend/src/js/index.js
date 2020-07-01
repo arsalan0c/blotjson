bea647b8392bd7e363c07afdca1e88488c62af40
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-07-02 05:06:34 +0800

    Add frontend improvements

--- src/frontend/src/js/index.js
+++ src/frontend/src/js/index.js
@@ -1,8 +1,8 @@
 const TOP_DIV = "main"; // id of the outer most div to which to add data
 
-const EXPAND_COLLAPSE_ALL_BTN = "expand-collapse-all-btn";
-const COLLAPSE_ALL_BTN_MSG = "Collapse All";
-const EXPAND_ALL_BTN_MSG = "Expand All";
+const EXPAND_ALL_BTN = "expand-all-btn";
+const COLLAPSE_ALL_BTN = "collapse-all-btn";
+
 const COLLAPSED_OBJ = " ... ";
 const CLASS_EXPANDED = "expanded";
 const CLASS_COLLAPSED = "collapsed";
@@ -20,14 +20,15 @@ webSocket.onmessage = (json) => {
     displayElement(el);
   });
 
-  registerExpandCollapseAll();
+  registerExpandAll();
+  registerCollapseAll();
 };
 
 /**
  * Renders an element, a distinct piece of data
  * @param {*} el The element to be visualised
  */
-const displayElement = (el) => {
+const displayElement = el => {
   const htmlElement = document.createElement("div");
   htmlElement.classList.add("element");
 
@@ -39,94 +40,98 @@ const displayElement = (el) => {
 /**
  * Renders (all forms of) data
  * @param {*} data The data to be visualised
- * @param {*} htmlElement The HTML element in which to visualise the data
+ * @param {*} parentElement The HTML element in which to visualise the data
  */
-const displayData = (data, htmlElement) => {
+const displayData = (data, parentElement) => {
   if (isObj(data)) {
-    displayObject(data, htmlElement);
+    displayObject(data, parentElement);
   } else if (Array.isArray(data)) {
-    displayArray(data, htmlElement);
+    displayArray(data, parentElement);
   } else {
-    displayValue(data, htmlElement);
+    displayValue(data, parentElement);
   }
 };
 
 /**
  * Renders an object
  * @param {*} obj The object to be visualised
- * @param {*} htmlElement The HTML element in which to visualise the object
+ * @param {*} parentElement The HTML element in which to visualise the object
  */
-const displayObject = (obj, htmlElement) => {
-  const objChildElement = document.createElement("div");
-  objChildElement.style.margin = "0 15px";
+const displayObject = (obj, parentElement) => {
+  const objElement = document.createElement("div");
+  objElement.style.margin = "0 15px";
 
   const keys = Object.keys(obj);
-  const expandFn = addCollapsible(htmlElement, objChildElement);
+  const expandFn = addCollapsible(objElement, parentElement);
 
-  objChildElement.appendChild(getMiscElement("{"));
+  objElement.appendChild(getMiscElement("{"));
   for (const key of keys) {
     const objEntryElement = getObjEntryElement(key);
+    objElement.appendChild(objEntryElement);
+
     displayData(obj[key], objEntryElement);
-
-    objChildElement.appendChild(objEntryElement);
   }
-  objChildElement.appendChild(getMiscElement("}"));
+  objElement.appendChild(getMiscElement("}"));
 
-  htmlElement.appendChild(objChildElement);
+  parentElement.appendChild(objElement);
   expandFn(true); // expand all collapsibles by default
 };
 
 /**
  * Renders an array
  * @param {*} arr The array to be visualised
- * @param {*} htmlElement The HTML element in which to visualise the array
+ * @param {*} parentElement The HTML element in which to visualise the array
  */
-const displayArray = (arr, htmlElement) => {
-  htmlElement.appendChild(getMiscElement("["));
+const displayArray = (arr, parentElement) => {
+  parentElement.appendChild(getMiscElement("["));
   for (const el of arr) {
-    displayData(el, htmlElement);
-    htmlElement.appendChild(getMiscElement(","));
+    displayData(el, parentElement);
+    parentElement.appendChild(getMiscElement(","));
   }
-  htmlElement.appendChild(getMiscElement("]"));
+  parentElement.appendChild(getMiscElement("]"));
 };
 
 /**
  * Renders a primitive value
  * @param {*} val The value to be visualised
- * @param {*} htmlElement The HTML element in which to visualise the value
+ * @param {*} parentElement The HTML element in which to visualise the value
  */
-const displayValue = (val, htmlElement) => {
-  htmlElement.appendChild(getValueElement(val));
+const displayValue = (val, parentElement) => {
+  parentElement.appendChild(getValueElement(val));
 };
 
 /**
  * Adds a button to expand/collapse a child HTML element
  * @param {*} value The value of the child HTML element
- * @param {*} parentDataElement The HTML element to which to add the button
- * @param {*} childDataElement The expanded HTML element
+ * @param {*} childElement The expanded HTML element
+ * @param {*} parentElement The HTML element to which to add the button
  * @returns A function for expanding or collapsing the child element
  */
-const addCollapsible = (parentDataElement, childDataElement) => {
+const addCollapsible = (childElement, parentElement) => {
   const collapsedElement = getCollapsedElement();
   const collapsibleElement = getCollapsibleElement();
 
-  let el = childDataElement;
+  let el = childElement;
 
   /**
    * Toggles the collapsible
    */
   function onClick() {
     collapsibleElement.classList.toggle(CLASS_EXPANDED);
+    repeat();
+  }
 
-    parentDataElement.removeChild(el);
+  function repeat() {
+    parentElement.removeChild(el);
     el = collapsibleElement.classList.contains(CLASS_EXPANDED)
-      ? childDataElement
+      ? childElement
       : collapsedElement;
-    parentDataElement.appendChild(el);
+    
+    parentElement.appendChild(el);
   }
 
   collapsibleElement.addEventListener("click", onClick);
-  parentDataElement.appendChild(collapsibleElement);
+  parentElement.appendChild(collapsibleElement);
 
   /**
    * Expands or collapses the collapsible depending on shouldExpand
@@ -139,11 +144,7 @@ const addCollapsible = (parentDataElement, childDataElement) => {
       collapsibleElement.classList.remove(CLASS_EXPANDED);
     }
 
-    parentDataElement.removeChild(el);
-    el = collapsibleElement.classList.contains(CLASS_EXPANDED)
-      ? childDataElement
-      : collapsedElement;
-    parentDataElement.appendChild(el);
+    repeat();
   }
 
   allCollapsibles.push(expandCollapsible);
@@ -175,7 +176,7 @@ const getCollapsedElement = () => {
  * @param {String} data Components of data that is not a key or value eg. a bracket
  * @returns A HTML element for data that is not a key or a value
  */
-const getMiscElement = (data) => {
+const getMiscElement = data => {
   const el = document.createElement("span");
   el.appendChild(document.createTextNode(data));
   isArrayBracket(data) || data === COLLAPSED_OBJ
@@ -189,13 +190,12 @@ const getMiscElement = (data) => {
  * @param {String} key
  * @returns A HTML element for an object entry
  */
-const getObjEntryElement = (key) => {
+const getObjEntryElement = key => {
   const el = document.createElement("div");
   const keyText = getKeyElement(key);
   el.appendChild(keyText);
   el.appendChild(getMiscElement(": "));
-  el.style.display = "flex";
-  el.style.flexDirection = "row";
+  el.classList.add("obj-entry");
   return el;
 };
 
@@ -203,7 +203,7 @@ const getObjEntryElement = (key) => {
  * @param {String} key
  * @returns A HTML element for a key
  */
-const getKeyElement = (key) => {
+const getKeyElement = key => {
   const el = document.createElement("span");
   el.appendChild(document.createTextNode(JSON.stringify(key)));
   el.classList.add("key");
@@ -214,7 +214,7 @@ const getKeyElement = (key) => {
  * @param {String} val
  * @returns A HTML element for a value
  */
-const getValueElement = (val) => {
+const getValueElement = val => {
   const el = document.createElement("span");
   el.appendChild(document.createTextNode(JSON.stringify(val)));
   el.classList.add("value");
@@ -228,21 +228,25 @@ const getValueElement = (val) => {
 const isArrayBracket = (str) => str === "[" || str === "]";
 
 /**
- * Registers the click handler for the button which expands/collapses all objects
+ * Registers the click handler for the button which expands all objects
  */
-const registerExpandCollapseAll = () => {
-  const btn = document.getElementById(EXPAND_COLLAPSE_ALL_BTN);
+const registerExpandAll = () => {
+  const btn = document.getElementById(EXPAND_ALL_BTN);
   btn.addEventListener("click", () => {
-    btn.innerHTML =
-      btn.innerHTML === COLLAPSE_ALL_BTN_MSG
-        ? EXPAND_ALL_BTN_MSG
-        : COLLAPSE_ALL_BTN_MSG;
-    allCollapsibles.forEach((expandCollapsible) => {
-      if (btn.innerHTML === COLLAPSE_ALL_BTN_MSG) {
-        expandCollapsible(true);
-      } else {
-        expandCollapsible(false);
-      }
+    allCollapsibles.forEach(expandCollapsible => {
+      expandCollapsible(true);
+    });
+  });
+};
+
+/**
+ * Registers the click handler for the button which collapses all objects
+ */
+const registerCollapseAll = () => {
+  const btn = document.getElementById(COLLAPSE_ALL_BTN);
+  btn.addEventListener("click", () => {
+    allCollapsibles.forEach(expandCollapsible => {
+      expandCollapsible(false);
     });
   });
 };
@@ -251,5 +255,5 @@ const registerExpandCollapseAll = () => {
  * @param {*} data
  * @returns true if data is an object. false otherwise.
  */
-const isObj = (data) =>
+const isObj = data =>
   typeof data === "object" && data !== null && !Array.isArray(data);
