d90a3d88c55f1781e67386464e4338c690cddcfe
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-07-07 00:29:18 +0800

    Update inline.js

--- src/frontend/inline.js
+++ src/frontend/inline.js
@@ -2,12 +2,13 @@ var Inliner = require('inliner');
 var fs = require('fs');
 
 const OUT_DIR = '../dist/';
+const OUT_HTML = 'index.html';
 
 new Inliner('./src/index.html', function (error, html) {
     if (error) {
         console.log(error);
     } else {
         fs.mkdirSync(OUT_DIR);
-        fs.writeFileSync(OUT_DIR + 'index.html', html);
+        fs.writeFileSync(OUT_DIR + OUT_HTML, html);
     }
 });
