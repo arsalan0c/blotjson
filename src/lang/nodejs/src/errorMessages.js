1adf05e4a68c39cf984828fb9db6b26dbfdb1a68
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-07-17 22:31:28 +0800

    Delete errorMessages.js

--- src/lang/nodejs/src/errorMessages.js
+++
@@ -1,5 +0,0 @@
-exports.INVALID_JSON_ERROR = 'Visualise must take in a valid JSON value';
-exports.NON_INTEGER_PORT_ERROR = 'Port must be a valid integer';
-exports.INVALID_PORT_NUMBER_ERROR = 'Invalid port number';
-exports.BAD_DATA_TRANSMISSION_ERROR =
-  'Data was not properly received by client';
