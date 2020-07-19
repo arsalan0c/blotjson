bb3138ebcfa2f651c12caae5670f9d65c188f2e6
Saad Haider <50513077+halcon-blanco@users.noreply.github.com>
2020-07-19 21:15:09 +0800

    Update errorMessages.js

--- src/lang/nodejs/src/errorMessages.js
+++ src/lang/nodejs/src/errorMessages.js
@@ -1,4 +1,4 @@
-exports.INVALID_JSON_ERROR = 'Invalid JSON text';
+exports.INVALID_JSON_ERROR = 'Invalid JSON value';
 exports.NON_INTEGER_PORT_ERROR = 'Port must be a valid integer';
 exports.INVALID_PORT_NUMBER_ERROR = 'Invalid port number';
 exports.BAD_DATA_TRANSMISSION_ERROR =
